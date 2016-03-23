9ae72ce458436127f3e0157934bc2706fb8270a1
Dominic Jodoin <cotsog@gmail.com>
2016-03-23 09:58:20 -0400

    first commit

---
+++ Tests/Tests.m
@@ -0,0 +1,45 @@
+//
+//  Tests.m
+//  Tests
+//
+//  Created by Dominic Jodoin on 2016-03-23.
+//  Copyright © 2016 Dominic Jodoin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    NSURLResponse *response;
+    NSError *error;
+    NSData *data = [NSURLConnection sendSynchronousRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.amazon.com"]]
+                                                                         returningResponse:&response
+                                                                         error:&error];
+    XCTAssertNotNil(data);
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ travis-ci-ssl-playground/travis_ci_ssl_playground.h
@@ -0,0 +1,13 @@
+//
+//  travis_ci_ssl_playground.h
+//  travis-ci-ssl-playground
+//
+//  Created by Dominic Jodoin on 2016-03-23.
+//  Copyright © 2016 Dominic Jodoin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface travis_ci_ssl_playground : NSObject
+
+@end
